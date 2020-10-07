072e41375db419eab2f4d122b2fa49c643f53254
Roman <37181628+T1kk3@users.noreply.github.com>
2020-10-07 10:11:08 +0300

    fix

--- Practice-2/Practice-2-4/Practice-2-4-2/Program.cs
+++ Practice-2/Practice-2-4/Practice-2-4-2/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.IO;
+using System.IO.IsolatedStorage;
 
-namespace Practice_2_4_2
+namespace pr_02_04_01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // При использовании GetMachineStoreForAssembly возникают проблемы с доступом,
+            // так что здесь хранилище будет создаваться в директории пользователя, а не системы
+
+            IsolatedStorageFile userStore = IsolatedStorageFile.GetUserStoreForAssembly();
+            IsolatedStorageFileStream userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Create, userStore);
+            StreamWriter userWriter = new StreamWriter(userStream);
+            userWriter.WriteLine("User Prefs");
+            userWriter.Close();
+
+            string[] files = userStore.GetFileNames("UserSettings.set");
+            if (files.Length == 0)
+            {
+                Console.WriteLine("File not found");
+            }
+            else
+            {
+                userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Open, userStore);
+                StreamReader userReader = new StreamReader(userStream);
+                string contents = userReader.ReadToEnd();
+                Console.WriteLine(contents);
+            }
         }
     }
 }
