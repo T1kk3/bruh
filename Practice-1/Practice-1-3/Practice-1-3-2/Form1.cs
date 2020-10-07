f3b9af86b6157160843b6dc14e465b3c1446d0b0
Roman <37181628+T1kk3@users.noreply.github.com>
2020-10-07 09:55:45 +0300

    done

---
+++ Practice-1/Practice-1-3/Practice-1-3-2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pr_01_03_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private System.Windows.Forms.Timer t;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            t = new Timer();
+            t.Interval = 1000;
+            t.Tick += new EventHandler(t_Tick);
+            t.Start();
+        }
+
+        void t_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value += 10;
+            if (progressBar1.Value >= 100)
+                t.Stop();
+        }
+        
+    }
+}
