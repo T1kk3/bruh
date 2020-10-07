b0543d38dcb0e122ed0d1699d1207402fce70075
Roman <37181628+T1kk3@users.noreply.github.com>
2020-10-07 10:09:48 +0300

    done

---
+++ Practice-2/Practice-2-4/Practice-2-4-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+namespace pr_02_04_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsolatedStorageFile userStore = IsolatedStorageFile.GetUserStoreForAssembly();
+            IsolatedStorageFileStream userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Create, userStore);
+            StreamWriter userWriter = new StreamWriter(userStream);
+            userWriter.WriteLine("User Prefs");
+            userWriter.Close();
+        }
+    }
+}
