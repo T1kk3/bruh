3175127e1966133916b7b7fe2bc42d7ab1f84e30
Roman <37181628+T1kk3@users.noreply.github.com>
2020-10-07 09:42:40 +0300

    Done

--- Practice-1/Practice-1-2/Practice-1-2-1/Program.cs
+++ Practice-1/Practice-1-2/Practice-1-2-1/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
-namespace Practice_1_2_1
+namespace pr_01_02_01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SByte a = 0;
+            Byte b = 0;
+            Int16 c = 0;
+            Int32 d = 0;
+            Int64 e = 0;
+            string s = "";
+            Exception ex = new Exception();
+            object[] types = { a, b, c, d, e, s, ex };
+            foreach (object o in types)
+            {
+                string type;
+                if (o.GetType().IsValueType)
+                    type = "Value type";
+                else
+                    type = "Reference type";
+                Console.WriteLine("{0}: {1}", o.GetType(), type);
+            }
         }
     }
 }
---
+++ Practice-1/Practice-1-2/Program-1-2-2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace pr_01_02_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Microsoft .NET Framework 2 Application Development Foundation";
+            string[] sa = s.Split(" ");
+            Array.Sort(sa);
+            s = String.Join(" ", sa);
+            Console.WriteLine(s);
+        }
+    }
+}
