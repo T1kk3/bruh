45991ad728098953e3e6825a5c1e8c18a91a30c2
Roman <37181628+T1kk3@users.noreply.github.com>
2020-10-07 10:02:35 +0300

    done

---
+++ Practice-2/Practice-2-1/Practice-2-1-1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace pr_02_01_01
+{
+    class Program
+    {
+        static void ShowDirectory(DirectoryInfo dir)
+        {
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                Console.WriteLine("File: {0}", file.FullName);
+            }
+
+            foreach (DirectoryInfo subDir in dir.GetDirectories())
+            {
+                ShowDirectory(subDir);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a path to directory you want to be shown: ");
+            string path = Console.ReadLine();
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(path);
+                ShowDirectory(dir);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error! Perhaps you've enter an invalid path?");
+            }
+        }
+    }
+}
