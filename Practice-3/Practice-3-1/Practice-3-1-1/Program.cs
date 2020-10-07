1d3da96979e54d8df03ce618fe0b781cddedaa2f
Roman <37181628+T1kk3@users.noreply.github.com>
2020-10-07 10:13:48 +0300

    done

---
+++ Practice-3/Practice-3-1/Practice-3-1-1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace pr_03_01_01
+{
+    class Program
+    {
+        static bool IsPhone(string s)
+        {
+            string pattern = @"^\(?\d{3}\)?[\s\-]?\d{3}\-?\d{4}$";
+            return Regex.IsMatch(s, pattern);
+        }
+
+        static bool IsZip(string s)
+        {
+            string pattern = @"^\d{5}(\-\d{4})?$";
+            return Regex.IsMatch(s, pattern);
+        }
+
+        static void Main(string[] args)
+        {
+            string[] testStrings = { "(555)555-1212", "(555) 555-1212", "555-555-1212", "5555551212", "01111", "01111-1111", "47", "111-11-1111" };
+            foreach (string s in testStrings)
+            {
+                if (IsPhone(s))
+                    Console.WriteLine("{0} is a phone", s);
+                else if (IsZip(s))
+                    Console.WriteLine("{0} is a zip", s);
+                else
+                    Console.WriteLine("{0} is unknown", s);
+            }
+        }
+    }
+}
