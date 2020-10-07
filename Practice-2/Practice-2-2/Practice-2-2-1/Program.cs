6b15c2c0c1e67274d3faba0bad5d7a24f5e66d54
Roman <37181628+T1kk3@users.noreply.github.com>
2020-10-07 10:06:04 +0300

    done

---
+++ Practice-2/Practice-2-2/Practice-2-2-1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace pr_02_02_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.CurrentDirectory + "/newfile.txt";
+            StreamWriter writer = File.CreateText(path);
+            writer.WriteLine("This is my new file");
+            writer.WriteLine("Do you like its format?");
+            writer.Close();
+        }
+    }
+}
