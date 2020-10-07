458a05e13925b18f3fbc0b7afe654bf97947f7f8
Roman <37181628+T1kk3@users.noreply.github.com>
2020-10-07 09:57:20 +0300

    done

---
+++ Practice-1/Practice-1-4/Practice-1-4-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace pr_01_04_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int16 i16 = 1;
+            Int32 i32 = 1;
+            double db = 1;
+            //i16 = i32; - неявное преобразование запрещено
+            i16 = (Int16)i32;
+            //i16 = db;  - неявное преобразование запрещено
+            i16 = (Int16)db;
+            i32 = i16;
+            //i32 = db;  - неявное преобразование запрещено
+            i32 = (Int32)db;
+            db = i16;
+            db = i32;
+        }
+    }
+}
