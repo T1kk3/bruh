ae2631f924d10027a129c1c7454d70d2f9e0d1e6
Roman <37181628+T1kk3@users.noreply.github.com>
2020-10-07 09:38:40 +0300

    Done

---
+++ Practice-1/Practice-1-1/Practice-1-1-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice_1_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
