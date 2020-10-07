c2cd7118955c14137ac8be804c1f7caa8f8629e7
Roman <37181628+T1kk3@users.noreply.github.com>
2020-10-07 09:34:11 +0300

    Done

--- Practice-1/Practice-1-1/Practice-1-1-1/Program.cs
+++ Practice-1/Practice-1-1/Practice-1-1-1/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
-namespace Practice_1_1_1
+namespace pr_01_01_01
 {
     class Program
     {
+        struct Person
+        {
+            public string firstName;
+            public string lastName;
+            public int age;
+            public Person(string _firstName, string _lastName, int _age)
+            {
+                firstName = _firstName;
+                lastName = _lastName;
+                age = _age;
+            }
+            public override string ToString()
+            {
+                return firstName + " " + lastName + ", age " + age;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person p = new Person("Tony", "Allen", 32);
+            Console.WriteLine(p);
         }
     }
 }
