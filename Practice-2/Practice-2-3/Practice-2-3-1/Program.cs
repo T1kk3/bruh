cb822380a53bbf13adb0564fd32cd881df4bb6a5
Roman <37181628+T1kk3@users.noreply.github.com>
2020-10-07 10:07:55 +0300

    done

---
+++ Practice-2/Practice-2-3/Practice-2-3-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace pr_02_03_01
+{
+    class Program
+    {
+        static void CompressFile(string inFilename, string outFilename)
+        {
+            FileStream sourceFile = File.OpenRead(inFilename);
+            FileStream destFile = File.OpenWrite(outFilename);
+            GZipStream compStream = new GZipStream(destFile, CompressionMode.Compress);
+            int theByte = sourceFile.ReadByte();
+            while (theByte != -1)
+            {
+                compStream.WriteByte((byte)theByte);
+                theByte = sourceFile.ReadByte();
+            }
+
+            compStream.Close();
+            sourceFile.Close();
+            destFile.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            string path_in = "/home/matt/test.txt";
+            string path_out = path_in + ".gz";
+            CompressFile(path_in, path_out);
+        }
+    }
+}
