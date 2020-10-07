79fb3f4a20d88d32321f6ff0974d976750227198
Roman <37181628+T1kk3@users.noreply.github.com>
2020-10-07 10:04:45 +0300

    dooone

---
+++ Practice-2/Practice-2-1/Practice-2-1-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace pr_02_01_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher("/var/log");
+            //watcher.Filter = "*.ini";
+            watcher.IncludeSubdirectories = true;
+            watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.Size;
+            watcher.Changed += new FileSystemEventHandler(watcher_Changed);
+            watcher.EnableRaisingEvents = true;
+            Console.Read();
+        }
+        static void watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("Changed: {0}", e.FullPath);
+        }
+    }
+}
